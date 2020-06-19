a031e7696c83a6063eee9c743d3d06c6f9c4bddf
tanxpyox <tanxpyox@gmail.com>
2020-06-19 17:46:51 +0000

    Deploying to release from  @ 68ca80c03847988e5f432969eae85e2b1359a040 🚀

--- codes/Bar Graph QJE 4.R
+++ codes/Bar Graph QJE 4.R
@@ -1,4 +1,4 @@
-# Bar Graph Grouped
+# Bar Graph, multi
 
 # David J. Deming, “The Growing Importance of Social Skills
 #   in the Labor Market,” The Quarterly Journal of Economics 132, no. 4 (2017):
@@ -7,32 +7,58 @@
 # Data: emulated from ibid, Fig 1
 
 library(ggplot2)
+library(dplyr)
+library(gridExtra)
+library(ggpubr)
 output_dir="output"
 
-df <- read.csv("data/cravino-2016-fig1.csv", stringsAsFactors = FALSE)
+df <- read.csv("data/deming-2017-emulated.dta", stringsAsFactors = F)
 
-df$class = factor(df$class, levels = rev(unique(df$class)))
-df$country = factor(df$country, levels = rev(unique(df$country)))
+df$occ <- factor(df$occ, levels = rev(unique(df$occ)))
 
-ggplot(df,aes(country,val,fill = class, color = class)) +
-  geom_bar(stat="identity",position = position_dodge()) +
-  scale_fill_manual(values = c('darkgrey', 'white')) +
-  scale_color_manual(values = c(NA, 'black')) +
-  scale_y_continuous(expand = c(0,0)) +
-  coord_flip() + theme_bw() +
+df1 <- filter(df, stem == 1)
+df2 <- filter(df, stem == 0)
+
+p1 <- ggplot(df1,aes(occ,diff)) +
+  geom_bar(stat="identity", fill = 	'#194670') +
+  ggtitle("STEM Occupations") +
+  coord_flip(ylim = c(-.2,.6)) +
+  theme_bw() +
   theme(
+    plot.title = element_text(hjust = 0.5),
     panel.grid.major.y = element_blank(),
     panel.grid.minor.x = element_blank(),
-    legend.title = element_blank(),
-    legend.position = "bottom",
     axis.title = element_blank(),
     axis.line = element_line(size=0.5),
-    panel.border=element_blank(),
-    legend.background = element_rect(color = 'black')
+    panel.border=element_blank()
   )
 
+
+p2<- ggplot(df2,aes(occ,diff)) +
+  geom_bar(stat="identity", fill = 	'#194670') +
+  ggtitle("All Other Managerial or Professional Occupations") +
+  coord_flip(ylim = c(-.2,.6)) +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    axis.title = element_blank(),
+    axis.line = element_line(size=0.5),
+    panel.border=element_blank()
+  )
+
+pc <- ggarrange(p1, p2,
+          ncol=1, nrow=2,
+          labels = "AUTO",
+          align = 'v')
+
 # Save individual Panels
-# ggsave(path=output_dir, filename="Bar Graph QJE 4.png", width=8, height=6)
+ggsave(path=output_dir, filename="Bar Graph QJE 4A.png", p1, width=8, height=6)
+ggsave(path=output_dir, filename="Bar Graph QJE 4B.png", p2, width=8, height=6)
+ggsave(path=output_dir, filename="Bar Graph QJE 4Combined.png", pc, width=8, height=12)
 
 # Save as Example
-# ggsave(path="src/others/frequency", filename="Bar Graph QJE 4-example.png", width=8, height=6)
+ggsave(path="src/others/frequency", filename="Bar Graph QJE 4A-example.png", p1, width=8, height=6)
+ggsave(path="src/others/frequency", filename="Bar Graph QJE 4B-example.png", p2, width=8, height=6)
+ggsave(path="src/others/frequency", filename="Bar Graph QJE 4Combined-example.png", pc, width=8, height=12)
