594331e48946c64013ba185521c3f2db4a325f1b
tanxpyox <tanxpyox@gmail.com>
2020-06-19 15:25:15 +0000

    Deploying to release from  @ 2c4006a06e934988e3d7d3480e1da79ecefb7231 🚀

--- codes/Scatter Plot APSR 1.R
+++ codes/Scatter Plot APSR 1.R
@@ -4,36 +4,50 @@
 #   The American Political Science Review, 110(3), 471.
 #   doi:10.1017/S0003055416000265
 
-# Data: Generated
+# Data: Eddy's Democracy Paper (protected)
 
 library(ggplot2)
 library(magrittr)
 library(gridExtra)
+library(haven)
+library(dplyr)
+library(countrycode)
 
 output_dir = "output"
-total = 100
 
+# total = 100
 # Generate dataset
-df <- data.frame(
-  x = rep(0,total),
-  y = rep(0,total),
-  name = rep(0,total))
+# df <- data.frame(
+#   x = rep(0,total),
+#   y = rep(0,total),
+#   name = rep(0,total))
+#
+# for (i in 1:total){
+#   df$x[i] = rnorm(1)
+#   df$y[i] = rnorm(1)
+#   df$name[i] = paste(LETTERS[sample(26,1)],LETTERS[sample(26,1)],LETTERS[sample(26,1)],sep = "")
+# }
 
-for (i in 1:total){
-  df$x[i] = rnorm(1)
-  df$y[i] = rnorm(1)
-  df$name[i] = paste(LETTERS[sample(26,1)],LETTERS[sample(26,1)],LETTERS[sample(26,1)],sep = "")
-}
+df <- read_dta("data/protected/eddy-democracy.dta") %>%
+  group_by(name) %>%
+  summarise(
+    x = mean(polity2),
+    y = mean(V141)
+  )
+
+# Convert country name to ISO country code
+df$name = countrycode(df$name, "country.name", "iso3c")
 
 # Plot
 ggplot(df, aes(x,y)) +
   geom_point() +
-  geom_text(aes(label = name), position = position_nudge(0.25)) +
-  geom_smooth(method = "lm", se=F, color = 'red') +
+  geom_text(aes(label = name), position = position_nudge(0.7)) +
+  geom_smooth(method = "gam", color = 'red') +
   labs(
-    x = "Democratic Index",
-    y = "GDP Growth Rate"
+    y = "Perceived Level of Democracy\n(1 = Least Democratic, 10 = Most Democratic)",
+    x = "Polity V (-10 = Least Democratic, 10 = Most Democratic)"
   ) +
+  scale_x_continuous(limits = c(-10,11)) +
   theme_bw() +
   theme(
     panel.grid.major = element_blank(),
@@ -46,4 +60,4 @@ ggplot(df, aes(x,y)) +
 ggsave(path=output_dir, filename="Scatter Plot APSR 1.png",width=8, height=6)
 
 # Save as example
-# ggsave(path="src/relationship", filename="Scatter Plot APSR 1Combined.png", width=8, height=6)
+ggsave(path="src/relationship", filename="Scatter Plot APSR 1Combined.png", width=8, height=6)
